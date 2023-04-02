925cd74186a64a5b407454884eac0106584fb26f
Carsten Thiele <software@carsten-thiele.de>
2023-04-02 10:56:34 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+# ch32v003 blink demo in rust
+
+Based on https://github.com/ch32-rs/ch32v203-demo/
+
+```shell
+
+cargo build -Zbuild-std=core --release
+
+riscv64-unknown-elf-objcopy -O binary target/riscv32i-unknown-none-elf/release/ch32v203-rust-blink ch32v003-rust-blink.bin
+
+minichlink -w ch32v003-rust-blink.bin flash
+
+minichlink -b
+
+```
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+#![no_std]
+#![no_main]
+
+use riscv_rt::entry;
+use panic_halt as _;
+use riscv as _;
+
+use ch32v0::ch32v003 as pac;
+
+#[no_mangle]
+fn default_start_trap() {
+
+}
+
+#[entry]
+fn main() -> ! {
+    let peripherals = pac::Peripherals::take().unwrap();
+    let rcc = peripherals.RCC;
+
+    rcc.apb2pcenr.modify(|_, w| w.iopcen().set_bit());
+
+    let gpioc = peripherals.GPIOC;
+
+    unsafe {
+        gpioc
+            .cfglr
+            .modify(|_, w| w.cnf4().bits(0b00).mode4().bits(0b11))
+    };
+
+    let cycle = 8_000_000 / 4;
+    loop {
+        gpioc.outdr.modify(|_, w| w.odr4().set_bit());
+        for _ in 0..cycle {
+            unsafe {
+                riscv::asm::nop();
+            }
+        }
+
+        gpioc.outdr.modify(|_, w| w.odr4().clear_bit());
+        for _ in 0..cycle {
+            unsafe {
+                riscv::asm::nop();
+            }
+        }
+    }
+}
+
